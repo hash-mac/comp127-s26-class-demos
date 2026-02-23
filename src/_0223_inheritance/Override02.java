8ed7df69cee8f79011dc32d45174fcb4e09786b3
Amin G. Alhashim, PhD, 6σ <aalhashi@macalester.edu>
2026-02-23 10:21:46 -0600

    Edit 02/23 class demo files

--- src/_0223_inheritance/Override02.java
+++ src/_0223_inheritance/Override02.java
@@ -3,22 +3,15 @@ package _0223_inheritance;
 public class Override02 {
     public static void main(String[] args) {
         P p = new C();
-        p.do2();
+        // p.do2();  // causes compile error.  WHY?
     }
 }
 
 class P {
-    public void do1() {
-
-    }
+    public void do1() {}
 }
 
 class C extends P {
-    public void do1() {
-
-    }
-
-    public void do2() {
-
-    }
+    public void do1() {}
+    public void do2() {}
 }
