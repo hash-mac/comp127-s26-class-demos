fd9990bd75e5b9a2c369c3cb9f22e3f99c917224
Amin G. Alhashim, PhD, 6σ <aalhashi@macalester.edu>
2026-03-27 09:14:37 -0500

    Update Sandbox.java

--- src/_0327_streams/Sandbox.java
+++ src/_0327_streams/Sandbox.java
@@ -3,3 +3,12 @@ package _0327_streams;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Collection;
+
+import _0325_streams.Car;
+
+public class Sandbox {
+    public static void main(String[] args) {
+        List<Car> l = new LinkedList<>();
+        Collection<Car> c;
+    }
+}
