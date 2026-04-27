5cca200bdb77277204be9673f8a15a5aa5f7b1dc
Amin Alhashim <aalhashi@macalester.edu>
2026-04-27 09:35:03 -0500

    Add 4/27 class demos

---
+++ src/_0427_keys/Keys.java
@@ -0,0 +1,13 @@
+package _0427_keys;
+
+import edu.macalester.graphics.CanvasWindow;
+import edu.macalester.graphics.Rectangle;
+
+public class Keys {
+    public static void main(String[] args) {
+        CanvasWindow cw = new CanvasWindow("Test Keys", 500, 500);
+        Rectangle rec = new Rectangle(0, 0, 20, 30);
+        rec.setCenter(cw.getHeight()/2, cw.getWidth()/2);
+        cw.add(rec);
+    }
+}
